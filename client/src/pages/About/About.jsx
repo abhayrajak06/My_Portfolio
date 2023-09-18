9940d978ffef72c2e643b9c98ca571bccd10b705
Abhay Rajak <mail2rajab01@gmail.com>
2023-09-18 21:33:54 +0530

    about me section added

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -4,11 +4,7 @@ import "./About.css";
 const About = ({ toggle }) => {
   return (
     <>
-      <div
-        className={
-          toggle ? "about bg-success adjust1" : "about bg-success adjust2"
-        }
-      >
+      <div className={toggle ? "about adjust1" : "about adjust2"}>
         <div className="container">
           <div className="row">
             <div className="col-md-6 about-pic">
@@ -17,7 +13,20 @@ const About = ({ toggle }) => {
                 alt="profile picture"
               />
             </div>
-            <div className="col-md-6">Content</div>
+            <div className="col-md-6 mt-2 about-content">
+              <h1 className="text-center">About Me</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Inventore, et! Itaque eligendi quidem ab veniam veritatis
+                tempora odit voluptas quis vero ipsa non, laborum exercitationem
+                quam vel nobis maxime sint inventore, suscipit, nihil velit ex
+                corporis! Delectus quasi dicta id accusamus minima error dolorum
+                aperiam. Necessitatibus provident ipsa suscipit consequatur
+                facere cum magnam eaque minima quod excepturi quos vel
+                cupiditate harum esse, nam saepe reiciendis tenetur nemo
+                possimus, ratione quibusdam.
+              </p>
+            </div>
           </div>
         </div>
       </div>
