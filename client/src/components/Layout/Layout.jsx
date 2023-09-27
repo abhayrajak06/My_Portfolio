71f155a8dc762d41daaaed036912e26e1b035115
Abhay Rajak <mail2rajab01@gmail.com>
2023-09-28 00:56:11 +0530

    changed the architecture for responsiveness purposes

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,45 +1,57 @@
-import React, { useState } from "react";
-import Home from "../../pages/Home/Home";
-import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from "react-icons/ai";
-import "./Layout.css";
+import React from "react";
 import Menu from "../Menu/Menu";
+import Home from "../../pages/Home/Home";
 import About from "../../pages/About/About";
 
 const Layout = () => {
-  const [toggle, setToggle] = useState(true);
-
-  //change toggle
-  const handleToggle = () => {
-    setToggle(!toggle);
-  };
-
   return (
-    <>
-      <div className="sidebar-section">
-        <div className={toggle ? "sidebar-toggle" : "sidebar"}>
-          <div className="sidebar-toggle-icons">
-            <p onClick={handleToggle}>
-              {toggle ? (
-                <AiOutlineDoubleLeft size={30} />
-              ) : (
-                <AiOutlineDoubleRight size={30} />
-              )}
-            </p>
-          </div>
-          <Menu toggle={toggle} />
+    <div className="container-fluid bg-warning">
+      <div className="row">
+        <div
+          className="col-md-2 col-sm-2 col-3"
+          style={{
+            height: "100vh",
+            position: "sticky",
+            top: 0,
+            backgroundColor: "#1e1e2c",
+          }}
+        >
+          <Menu />
         </div>
-        <div className="info-section">
-          <div
-            className={
-              toggle ? "info-section-toggle" : "info-section-not-toggle"
-            }
-          >
-            <Home toggle={toggle} />
-            <About toggle={toggle} />
+        <div
+          className="col-sm-10 col-md-10 col-9 p-0"
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <div className="">
+            <Home />
+          </div>
+          <div className="">
+            <About />
+          </div>
+          <div className="bg-secondary" style={{ height: "12rem" }}>
+            hello
+          </div>
+          <div className="bg-success" style={{ height: "12rem" }}>
+            hello
+          </div>
+          <div className="bg-danger" style={{ height: "12rem" }}>
+            hello
+          </div>
+          <div className="bg-secondary" style={{ height: "12rem" }}>
+            hello
+          </div>
+          <div className="bg-success" style={{ height: "12rem" }}>
+            hello
+          </div>
+          <div className="bg-danger" style={{ height: "12rem" }}>
+            hello
+          </div>
+          <div className="bg-secondary" style={{ height: "12rem" }}>
+            hello
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
