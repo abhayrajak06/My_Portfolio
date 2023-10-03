779fa7afcd2fddc8f14ccf6beac54433bc6f9d98
Abhay Rajak <mail2rajab01@gmail.com>
2023-10-03 13:32:09 +0530

    node server initialized for backend work

---
+++ controllers/portfolioController.js
@@ -0,0 +1,15 @@
+export const sendEmailController = async (req, res) => {
+  try {
+    return res.status(200).send({
+      success: true,
+      message: "Your message send Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Send Email API Error",
+      error,
+    });
+  }
+};
